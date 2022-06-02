5213723701ded275d26d608a4621750c83dec371
Tim Slager <timtendo12@gmail.com>
2022-06-02 16:10:23 +0200

    added currecny

--- resources/views/components/currency.blade.php
+++ resources/views/components/currency.blade.php
@@ -2,31 +2,33 @@
     <div id="bar" class="w-[195px] h-8 my-[5px] ml-[35px] mr-[30px] float-left rounded-[5px]">
         <div class="ml-[3px] rounded-[15px] h-[30px] w-[30px] float-left" style="background-image: url('/img/icons/credits.png'); background-repeat: no-repeat; background-color: #b68c1d; background-position-x: center; background-position-y: center; box-shadow: inset 0 0 0 2px #e9e9e9;">
         </div>
-        <b class="ml-[3px] text-slate-800 float-left font-size-[12px] leading-[30px]">crdts.amount</b>
+        <b class="ml-[3px] text-slate-800 float-left font-size-[12px] leading-[30px]">0</b>
         <span class="float-right text-slate-800 leading-[30px] font-size-[12px]">credits</span>
     </div>
     <div id="bar" class="w-[195px] h-8 my-[5px] ml-[35px] mr-[30px] float-left rounded-[5px]">
-        <div class="ml-[3px] rounded-[15px] h-[30px] w-[30px] float-left" style="background-image: url('/img/icons/credits.png'); background-repeat: no-repeat; background-color: #985c9b; background-position-x: center; background-position-y: center; box-shadow: inset 0 0 0 2px #e9e9e9;">
+        <div class="ml-[3px] rounded-[15px] h-[30px] w-[30px] float-left" style="background-image: url('/img/icons/duckets.png'); background-repeat: no-repeat; background-color: #985c9b; background-position-x: center; background-position-y: center; box-shadow: inset 0 0 0 2px #e9e9e9;">
         </div>
-        <b class="ml-[3px] text-slate-800 float-left font-size-[12px] leading-[30px]">dcks.amount</b>
+        <b class="ml-[3px] text-slate-800 float-left font-size-[12px] leading-[30px]">0</b>
         <span class="float-right text-slate-800 leading-[30px] font-size-[12px]">duckets</span>
     </div>
     <div id="bar" class="w-[195px] h-8 my-[5px] ml-[35px] mr-[30px] float-left rounded-[5px]">
-        <div class="ml-[3px] rounded-[15px] h-[30px] w-[30px] float-left" style="background-image: url('/img/icons/credits.png'); background-repeat: no-repeat; background-color: #77a2a2; background-position-x: center; background-position-y: center; box-shadow: inset 0 0 0 2px #e9e9e9;">
+        <div class="ml-[3px] rounded-[15px] h-[30px] w-[30px] float-left" style="background-image: url('/img/icons/diamonds.png'); background-repeat: no-repeat; background-color: #77a2a2; background-position-x: center; background-position-y: center; box-shadow: inset 0 0 0 2px #e9e9e9;">
         </div>
-        <b class="ml-[3px] text-slate-800 float-left font-size-[12px] leading-[30px]">dmt</b>
+        <b class="ml-[3px] text-slate-800 float-left font-size-[12px] leading-[30px]">0</b>
         <span class="float-right text-slate-800 leading-[30px] font-size-[12px]">diamanten</span>
     </div>
     <div id="bar" class="w-[195px] h-8 my-[5px] ml-[35px] mr-[30px] float-left rounded-[5px]">
-        <div class="ml-[3px] rounded-[15px] h-[30px] w-[30px] float-left" style="background-image: url('/img/icons/credits.png'); background-repeat: no-repeat; background-color: #6c9c67; background-position-x: center; background-position-y: center; box-shadow: inset 0 0 0 2px #e9e9e9;">
+        <div class="ml-[3px] rounded-[15px] h-[30px] w-[30px] float-left" style="background-image: url('/img/icons/belcredits.png'); background-repeat: no-repeat; background-color: #6c9c67; background-position-x: center; background-position-y: center; box-shadow: inset 0 0 0 2px #e9e9e9;">
         </div>
-        <b class="ml-[3px] text-slate-800 float-left font-size-[12px] leading-[30px]">bc.amount</b>
+        <b class="ml-[3px] text-slate-800 float-left font-size-[12px] leading-[30px]">0</b>
         <span class="float-right text-slate-800 leading-[30px] font-size-[12px]">bel-credits</span>
     </div>
-    <div id="bar" class="float-right">
-        <div id="bar" class="w-26 h-8 my-[5px] ml-[35px] mr-[30px] float-left rounded">
-            <div id="iconstatus" class="bg-green-500">
+    <div class="float-right">
+        <div id="bar" class="w-[195px] h-8 my-[5px] ml-[35px] mr-[30px] float-left rounded-[5px]">
+            <div id="iconstatus" class="bg-green-500 ml-[3px] rounded-[9px] h-[18px] w-[18px] my-[5px] float-left">
             </div>
+            <b class="ml-[3px] text-slate-800 float-left font-size-[12px] leading-[30px]">0</b>
+            <span class="float-right text-slate-800 leading-[30px] font-size-[12px]">vrienden online</span>
         </div>
     </div>
 </div>
