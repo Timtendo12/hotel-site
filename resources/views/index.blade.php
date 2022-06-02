56ae5f42c64c69281613b02017292e0e2127cdf1
Tim Slager <timtendo12@gmail.com>
2022-06-02 10:41:41 +0200

    tailwindcss init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <title>hotel-site</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<p class="hidden">Tailwind does not work yet :(</p>
+<p class="text-lime-400">Green Hello world!</p>
+</body>
+</html>
